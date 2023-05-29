b2f383eec1144a6685a7b47b295e9332c126ff99
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2023-05-29 20:11:59 +0800

    ok

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -11,36 +11,47 @@ export default function Signup() {
           <div className="text-secondary">Welcome to J's Chat.</div>
           <div className="signup-form">
             <label className="username" htmlFor="username">
-              <span></span>
               <input type="text" name="username" placeholder="Username" required />
             </label>
             <label className="password" htmlFor="password">
-              <span></span>
               <input type="password" name="password" placeholder="Password" required/>
             </label>
             <label className="firstname" htmlFor="firstname">
-              <span></span>
               <input type="text" name="firstname" placeholder="First Name" required />
             </label>
             <label className="middlename" htmlFor="middlename">
-              <span></span>
               <input type="text" name="middlename" placeholder="Middle Name" />
             </label>
             <label className="lastname" htmlFor="lastname">
-              <span></span>
               <input type="text" name="lastname" placeholder="Last Name" required />
             </label>
             {/* <!-- Birthday --> */}
-            {/* <!-- Gender --> */}
-            {/* <!-- --> */}
-            <label className="" htmlFor="username">
-              <span></span>
-              <input type="text" name="username" placeholder="Username" required />
+            <label className="birthday" htmlFor="birthday">
+              <input type="date" name="birthday" placeholder="Birthday" required />
             </label>
-
+            {/* <!-- Gender --> */}
+            <label className="gender" htmlFor="gender">
+              <select name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </label>
+            {/* <!-- Civil Status --> */}
+            <label className="civilstatus" htmlFor="civilstatus">
+              <select name="civilstatus">
+                <option value="Single">Single</option>
+                <option value="Married">Married</option>
+                <option value="Widowed">Widowed</option>
+              </select>
+            </label>
+            <label className="address" htmlFor="address">
+              <input type="text" name="address" placeholder="Address" />
+            </label>
+            <label className="aboutme" htmlFor="aboutme">
+              <input type="text" name="aboutme" placeholder="About Me" />
+            </label>
             <button className="signup-button">Sign up</button>
           </div>
-          <hr />
           <div className="login-content">
             Already have an account? <a href="/login">Login here!</a>
           </div>
